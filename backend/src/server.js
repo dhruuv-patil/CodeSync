e1f99d6707ec0ffba95ea63f1778a90c6f118c00
Dhruv Patil <dhruvpatil@Dhruvs-MacBook-Air.local>
2026-05-17 14:23:05 +0530

    Cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,17 +20,19 @@ app.get("/test", (req, res) => {
   res.json({ message: "Backend works" });
 });
 
-app.use(express.json())
-app.use((req, res, next) => {
-  console.log("REQUEST:", req.method, req.url);
-  next();
-});
 app.use(
   cors({
     origin: true,
     credentials: true,
   })
 );
+
+app.use(express.json())
+app.use((req, res, next) => {
+  console.log("REQUEST:", req.method, req.url);
+  next();
+});
+
 
 app.use(clerkMiddleware())
 
